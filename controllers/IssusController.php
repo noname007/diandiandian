15f317f23e15fb90bb3f2410de45d78fb87e5b7c
y.zz <yz0437@gmail.com>
2016-03-03 16:23:50 +0800

    显示员工同事

--- controllers/IssusController.php
+++ controllers/IssusController.php
@@ -5,10 +5,12 @@ use yii\web\Controller as WebController;
 use app\models\FillIssusInfo;
 use yii\helpers\VarDumper;
 use app\models\IssusMember;
+use yii\db\Query;
+use app\models\IssusUser;
 
 class IssusController extends WebController
 {
-	public $defaultAction = 'create'; 
+	public $defaultAction = 'member'; 
 	
 	public function actionIndex()
 	{
@@ -78,5 +80,29 @@ class IssusController extends WebController
 	{
 		return 'display meme/memu';
 	}
+	
+	
+	
+	function actionMember()
+	{
+	    $user_id = \Yii::$app->user->getId();
+	    
+	    $tableName = IssusMember::tableName();
+	    $subsql1 = 'select issus_id from '. $tableName.' where user_id=:user_id';
+	    
+	    $subsql2 = "select t1.user_id  from  $tableName t1, ($subsql1) as t2 where t1.issus_id = t2.issus_id";
+	    
+	    $sql = "select username from ($subsql2) t3 , user t4 where t4.id = t3.user_id and t4.flags = 0";
+	    
+	    $bindValues =[
+	        ':user_id'=>$user_id
+	    ];
+	    $res = \Yii::$app->db->createCommand($sql,$bindValues)->queryAll();
+	   
+	    
+	    //TODO 具体显示
+	    var_dump($res);
+	}
+	
 	
 }
